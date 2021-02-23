2509e6e872b78cb5e74f035457b8e2e62cc9c968
MSadoon94 <sadoonmike@gmail.com>
2021-02-23 10:32:32 -0700

    Add function to NegativeFormatter
    Finding and Tagging negative numbers will be necessary to reduce errors
    downstream when attempting to find all operations in the request.

--- src/main/java/org/calculator/processing/NegativeFormatter.java
+++ src/main/java/org/calculator/processing/NegativeFormatter.java
@@ -2,8 +2,20 @@ package org.calculator.processing;
 
 import org.calculator.common.Request;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NegativeFormatter {
 	public Request tagNegatives(Request request) {
-		return null;
+		Pattern negativePattern = Pattern.compile("(\\D-)");
+		Matcher negativeMatcher = negativePattern.matcher(request.value());
+		boolean hasANegativeNumber = negativeMatcher.find();
+		Request formatted;
+		formatted = (hasANegativeNumber) ? tag(request, negativeMatcher.group()) : request;
+		return formatted;
+	}
+	private Request tag(Request request, String section){
+		String modifiedSection = section.substring(0, 1);
+		return new Request(request.value().replace(section, modifiedSection + "NEG"));
 	}
 }
