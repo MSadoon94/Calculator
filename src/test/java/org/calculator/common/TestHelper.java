6dc36b43f25edaa4fbfccb3d98a701e312f8411c
MSadoon94 <sadoonmike@gmail.com>
2021-03-25 06:42:33 -0600

    Add scientific notation feature
    Clicking the notation button while a number is in the text area will
    return the scientific notation of the number.

--- src/test/java/org/calculator/common/TestHelper.java
+++ src/test/java/org/calculator/common/TestHelper.java
@@ -13,7 +13,8 @@ public enum TestHelper {
 	EXPONENT("2^3", bigDecimals(2, 3), "8.00"),
 	SQUARE_ROOT("√4", bigDecimals(4), "2.00"),
 	NEGATIVE("4+-2", bigDecimals(4, -2), "2.00"),
-	DECIMAL("4.5234*5.4325", bigDecimals(4.5234, 5.4325), "24.573");
+	DECIMAL("4.5234", bigDecimals(4.5234, 5.4325), "4.523"),
+	NOTATION("6543.21", bigDecimals(6543.21), "6.54321*10^3");
 
 	private final String input;
 	private final BigDecimal[] extracted;
