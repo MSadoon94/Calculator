3d8ef20751ddaf2efcf3552c98fe89285146f79b
MSadoon94 <sadoonmike@gmail.com>
2021-03-31 10:42:42 -0600

    Remove unnecessary dependencies

--- src/main/java/org/calculator/verification/ErrorSender.java
+++ src/main/java/org/calculator/verification/ErrorSender.java
@@ -1,19 +1,12 @@
 package org.calculator.verification;
 
-import org.calculator.common.Operations;
-import org.calculator.common.Request;
 import org.calculator.user.Ui;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class ErrorSender {
-	private InputValidator validator;
 	private Ui gui;
 
-	public ErrorSender(InputValidator aValidator, Ui aGui){
-		validator = aValidator;
+	public ErrorSender(Ui aGui){
 		gui = aGui;
 	}
 
