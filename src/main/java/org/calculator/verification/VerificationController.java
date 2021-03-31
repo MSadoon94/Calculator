2afe6358f450d84c04a9f08ef5022613fa4b5fd7
MSadoon94 <sadoonmike@gmail.com>
2021-03-31 10:42:42 -0600

    Remove unnecessary dependencies

--- src/main/java/org/calculator/verification/VerificationController.java
+++ src/main/java/org/calculator/verification/VerificationController.java
@@ -4,14 +4,10 @@ import org.calculator.user.Ui;
 
 public class VerificationController implements VerificationBoundary {
 
-	public ErrorSender errorSender(Ui aGui) {
-		return new ErrorSender(new InputValidator(), aGui);
-	}
-
 	public InputVerifier verifier(Ui gui) {
 		InputValidator validator = new InputValidator();
 		return new InputVerifier(
 				validator,
-				new ErrorSender(validator, gui));
+				new ErrorSender(gui));
 	}
 }
